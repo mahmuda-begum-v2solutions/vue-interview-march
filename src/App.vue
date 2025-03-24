61e5cf9be5439b56fd752de5b2bd9a874a8a0adb
Mahmuda Begum <mahmuda.begum@v2solutions.com>
2025-03-24 21:48:26 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="header">
+    <nav class="nav">
+      <RouterLink to="/" class="link">
+        Home
+      </RouterLink>
+      <RouterLink to="/about" class="link">
+        About
+      </RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 0;
+}
+
+.nav {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 3rem;
+  font-size: 0.875rem;
+}
+
+.link {
+  padding: 0.75rem 1.5rem;
+  color: #4a4a4a;
+  text-decoration: none;
+}
+
+.link:hover {
+  color: #1a1a1a;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">
+    <h1 class="title">This is home page</h1>
+  </main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.title {
+  font-size: 1.5rem;
+  /* Equivalent to text-2xl */
+  font-weight: bold;
+  color: red;
+  /* Equivalent to text-red-500 */
+}
+</style>
